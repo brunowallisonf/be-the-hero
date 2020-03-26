f5f43fc39accbae0c01fb626f610bf3896ead44d
Bruno Wallison <brunowallison.fernandes@gmail.com>
2020-03-25 22:20:41 -0300

    Application back-end

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const routes = require("./routes");
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
